afb93a24ab2f5127bd8ef9397d42f72f134d51af
Pouya <pouyamohammadi.work@gmail.com>
2025-08-13 22:08:51 +0330

    opt: bundling server with es build to minify output

--- service/index.js
+++ service/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import basicAuth from "express-basic-auth";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const users = {};
 if (process.env?.USERNAME && process.env?.PASSWORD)
